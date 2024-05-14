f624571012d1427a15f1acb5e7951cb7da719ab2
nkkho <160609424+nkkho@users.noreply.github.com>
2024-05-14 12:06:20 +0800

    a better queststate change

--- test2/agent_test2.py
+++ test2/agent_test2.py
@@ -15,14 +15,14 @@ except json.JSONDecodeError as e:
 #character initialization
 blacksmith = Villager(agent1_config[0],"WEREWOLF CLAW", {"WEAPON":1, "VILLAGE A BLACKSMITH LETTER":1},"VILLAGE A")
 player = Player(agent1_config[1],"VILLAGE A")
-gamemaster = GameMaster([blacksmith],player)
+gamemaster = GameMaster()
 #merchant = Villager(agent1_config[2],None,{"COIN":100},"VILLAGE A")
 
 gamemaster.agent = autogen.AssistantAgent(name="GameMaster",llm_config={"config_list":config_list},system_message="You are the game master of this game, you should make decision based on the chat_message and intruction given")
 werewolf = Monster(5)
 bsystem_message = generate_prompt(list(blacksmith.prompt_input.values()),blacksmith.config["message_path"])
 blacksmith.agent = autogen.AssistantAgent(name = blacksmith.config["name"], system_message=bsystem_message,llm_config={"config_list":config_list})
-player.agent = autogen.UserProxyAgent(name = player.config["name"], system_message=player.config["system_message"], llm_config={"config_list":config_list},
+player.agent = autogen.UserProxyAgent(name = player.config["name"], system_message=player.config["system_message1"], llm_config={"config_list":config_list},
                                       code_execution_config={"work_dir": "groupchat", "timeout":120, "last_n_messages":2,"use_docker": False})
 #merchant.agent = autogen.AssistantAgent(name = merchant.config["name"], system_message=merchant.config["system_message1"],llm_config={"config_list":config_list})
 max_round: Final[int] = 15
@@ -39,40 +39,50 @@ while round < max_round:
         message = None
         while round < max_round:
             bquest_state=gamemaster.quest_state(blacksmith,player)
+            print(f"master state answer: {bquest_state}")
             if "NOT GIVEN" in bquest_state:
                 blacksmith.switchqueststate("NOT GIVEN")
             elif "GIVEN" in bquest_state:
                 blacksmith.switchqueststate("GIVEN")
+                player.add_quest(blacksmith)
+                player.agent.update_system_message(player.config["system_message2"])
+            elif "SUCCESS" in bquest_state:
+                blacksmith.switchqueststate("SUCCESS")
             elif "COMPLETE" in bquest_state:
                 blacksmith.switchqueststate("COMPLETE")
+                if blacksmith.config["job"] in player.quest.keys():
+                    player.quest.pop(blacksmith.config["job"])
+            print(f"blacksmith queststate: {blacksmith.quest_state}")
             if speaker==None:
                 message = input("Player: ")
                 if message=="END":
                     speaker = None
                     break
-                player.broadcasting([blacksmith.agent,gamemaster.agent],message)
+                player.broadcasting([blacksmith.agent],message)
+                gamemaster.joinConversation(player,blacksmith,f"Player: {message}")
                 speaker = blacksmith
             elif speaker==player:
                 message = player.agent.generate_reply(sender=blacksmith.agent)
-                print(f"{speaker.config['name']}: {message['content']}")
-                player.broadcasting([blacksmith.agent,gamemaster.agent],message)
+                if type(message)!=str:
+                    output = message["content"]
+                else:
+                    output = message
+                print(f"{speaker.config['name']}: {output}")
+                player.broadcasting([blacksmith.agent],message)
+                gamemaster.joinConversation(player,blacksmith,f"{speaker.config['name']}: {message['content']}")
                 if message["content"]=="END":
                     print("breaking")
                     speaker = None
                     break
                 speaker = blacksmith
             elif speaker==blacksmith:
-                print(f"blacksmith queststate: {blacksmith.quest_state}")
-                if blacksmith.quest_state=="COMPLETE":
+                if blacksmith.quest_state=="SUCCESS" and blacksmith.reward_state > 0:
                     rewardgiving=blacksmith.rewardGiving()
                     print(rewardgiving)
                     if rewardgiving=="REWARD":
                         player.mergeDict(blacksmith.reward)
                         player.updateItems(blacksmith.request, -1)
                         blacksmith.reward_state = 0
-                        blacksmith.quest_state = "NOT GIVEN"
-                        blacksmith.prompt_input["quest_state"] = blacksmith.quest_state
-                        blacksmith.update_system_message(blacksmith.config["message_path"],list(blacksmith.prompt_input.values()))
                         print("reward given")
                         if "WEREWOLF CLAW" in player.items.keys():
                             print("num_claws: "+str(player.items["WEREWOLF CLAW"]))
@@ -81,7 +91,8 @@ while round < max_round:
                         print("player_item:" + str(player.items))
                 message = blacksmith.agent.generate_reply(sender=player.agent)
                 print(f"{speaker.config['name']}: {message}")
-                blacksmith.broadcasting([player.agent,gamemaster.agent],message)
+                gamemaster.joinConversation(player,blacksmith,f"{speaker.config['name']}: {message}")
+                blacksmith.broadcasting([player.agent],message)
 
                 if message=="END":
                     speaker = None
@@ -93,7 +104,7 @@ while round < max_round:
         result = gamemaster.fight(player.strength,werewolf.strength)
         if result == "WIN":
             player.updateItems("WEREWOLF CLAW",1)
-            gamemaster.broadcasting([player.agent,blacksmith.agent],"Player kills a werewolf")
+            gamemaster.broadcasting([player.agent,blacksmith.agent],"Player kills a werewolf and get a WEREWOLF CLAW")
             print("Player kills a werewolf")
             print("num_claw: "+ str(player.items["WEREWOLF CLAW"]))
     elif action == "VILLAGE A":
